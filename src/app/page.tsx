4648cffd90567d135ab3141dca96122438d0edc5
zikrifikri21 <iniakunku329@gmail.com>
2025-06-29 20:15:01 +0800

    fix bug: icon from psge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,9 @@ import FlowingMenu from "@/components/Kegiatan";
 import MemoriesSection from "@/components/MemoriesSection";
 import ParallaxSection from "@/components/ParallaxSection";
 import Image from "next/image";
+import React from "react";
+import { AiBrain03Icon, SoftwareLicenseIcon, NetworkIcon } from "./AiBrain03Icon";
+
 const demoItems = [
   { link: "#", text: "Praktikum Fisika", image: "/assets/img/6.jpg" },
   { link: "#", text: "Praktikum Biologi", image: "/assets/img/7.jpg" },
@@ -125,32 +128,3 @@ export default function Home() {
     </>
   );
 }
-import React from "react";
-
-export const AiBrain03Icon = ({ size = 24, color = "currentColor", ...props }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={size} height={size} fill={color} {...props}>
-    <g xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" color="currentColor">
-      <path d="M4 16.5a3 3 0 0 0 3 3a2.5 2.5 0 0 0 5 0a2.5 2.5 0 1 0 5 0a3 3 0 0 0 2.567-4.553a3.001 3.001 0 0 0 0-5.893A3 3 0 0 0 17 4.5a2.5 2.5 0 1 0-5 0a2.5 2.5 0 0 0-5 0a3 3 0 0 0-2.567 4.553a3.001 3.001 0 0 0 0 5.893A3 3 0 0 0 4 16.5" />
-      <path d="m7.5 14.5l1.842-5.526a.694.694 0 0 1 1.316 0L12.5 14.5m3-6v6m-7-2h3" />
-    </g>
-  </svg>
-);
-
-export const SoftwareLicenseIcon = ({ size = 24, color = "currentColor", ...props }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={size} height={size} fill={color} {...props}>
-    <g xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" color="currentColor">
-      <path d="M2.5 12c0-4.478 0-6.718 1.391-8.109S7.521 2.5 12 2.5c4.478 0 6.718 0 8.109 1.391S21.5 7.521 21.5 12c0 4.478 0 6.718-1.391 8.109S16.479 21.5 12 21.5c-4.478 0-6.718 0-8.109-1.391S2.5 16.479 2.5 12m0-3h19M7 6h.009M11 6h.009" />
-      <path d="m8 13l1.227 1.057c.515.445.773.667.773.943s-.258.498-.773.943L8 17m4 0h4" />
-    </g>
-  </svg>
-);
-
-export const NetworkIcon = ({ size = 24, color = "currentColor", ...props }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" width={size} height={size} fill={color} {...props}>
-    <path
-      xmlns="http://www.w3.org/2000/svg"
-      fill="currentColor"
-      d="M992 512H832v256h64q26 0 45 19t19 45v128q0 27-19 45.5t-45 18.5H704q-27 0-45.5-18.5T640 960V832q0-26 18.5-45t45.5-19h64V512H320v256h64q26 0 45 19t19 45v128q0 27-19 45.5t-45 18.5H192q-27 0-45.5-18.5T128 960V832q0-26 18.5-45t45.5-19h64V512H32q-13 0-22.5-9.5T0 480t9.5-22.5T32 448h480V256h-64q-27 0-45.5-18.5T384 192V64q0-26 18.5-45T448 0h192q26 0 45 19t19 45v128q0 27-19 45.5T640 256h-64v192h416q13 0 22.5 9.5t9.5 22.5t-9.5 22.5T992 512z"
-    />
-  </svg>
-);
